441a8711d32328a2ff4f33577bc3dfa5e4c88fcb
Samy KACIMI <samy.kacimi@gmail.com>
2015-09-19 13:20:49 +0200

    add mobile, location to hash. add client test coverage

--- lib/omniauth/strategies/marvin.rb
+++ lib/omniauth/strategies/marvin.rb
@@ -3,7 +3,7 @@ require "omniauth/strategies/oauth2"
 module OmniAuth
   module Strategies
     class Marvin < OmniAuth::Strategies::OAuth2
-      option :name, :marvin
+      option :name, "marvin"
 
       option :client_options, {
         site: "https://api.intrav2.42.fr",
@@ -18,12 +18,14 @@ module OmniAuth
           login: raw_info["login"],
           url: raw_info["url"],
           name: raw_info["displayname"],
+          mobile: raw_info["mobile"],
           image: raw_info["image_url"],
           staff?: raw_info["staff?"],
           correction_point: raw_info["correction_point"],
           wallet: raw_info["wallet"],
           level: raw_info["cursus"]["level"],
-          grade: raw_info["cursus"]["grade"]
+          grade: raw_info["cursus"]["grade"],
+          location: raw_info["location"]
         }
       end
 
