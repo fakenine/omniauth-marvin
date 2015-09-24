e3f2315dcf073ca108a00745b83fb5ec6c860a4d
Samy KACIMI <samy.kacimi@gmail.com>
2015-09-24 12:47:48 +0200

    remove simple cov test

--- Gemfile
+++ Gemfile
@@ -10,5 +10,4 @@ group :test do
   gem 'rack-test'
   gem 'webmock'
   gem 'simplecov', require: false
-  gem "codeclimate-test-reporter", require: nil
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,5 @@
 require "bundler/setup"
 require 'simplecov'
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
 SimpleCov.start
 require "rspec"
 require "rack/test"
@@ -10,7 +8,7 @@ require "omniauth"
 require "omniauth-marvin"
 
 RSpec.configure do |config|
-  
+
   config.include WebMock::API
   config.include Rack::Test::Methods
   config.extend OmniAuth::Test::StrategyMacros, type: :strategy
