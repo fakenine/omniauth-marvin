84e0714d3a6bae97e6939aeefbcb28fb3deccc2d
Samy KACIMI <samy.kacimi@gmail.com>
2015-09-24 10:21:16 +0200

    callback path test

--- spec/omniauth/strategies/omniauth_marvin_spec.rb
+++ spec/omniauth/strategies/omniauth_marvin_spec.rb
@@ -13,16 +13,22 @@ describe OmniAuth::Strategies::Marvin do
   end
 
   describe "client options" do
-    it 'has a valid name' do
+    it 'has a the correct name' do
       expect(subject.options.name).to eq("marvin")
     end
 
-    it 'has a valid site' do
+    it 'has the correct site' do
       expect(subject.options.client_options.site).to eq("https://api.intrav2.42.fr")
     end
 
-    it "has a valid auth url" do
+    it "has the correct auth url" do
       expect(subject.options.client_options.authorize_path).to eq("v2/oauth/authorize")
+    end
+  end
+
+  describe 'callback path' do
+    it 'has the correct callback path' do
+      expect(subject.callback_path).to eq('/auth/marvin/callback')
     end
   end
 end
